6456a776262e603451caa138284c858eb3d028c0
JFWolters <34782627+JFWolters@users.noreply.github.com>
2018-02-19 18:38:44 -0500

    Initial Upload 2.0.1
    Initial Upload of Robot Image Analysis 2.0.1

---
+++ README.md
@@ -0,0 +1,1 @@
+# Robot-Image-Analysis-master
---
+++ Robot Image Analysis 2.0.1/LogisticGrowth/remove_rate_outliers.R
@@ -0,0 +1,63 @@
+####Detect outlier spots within individual blocks, and remove them
+####These outlier spots are likely caused by issues within the source plates
+
+library(dplyr)
+
+modeled_file = "logistic_growth.omit_errors_lowcorr.tab"
+
+classes = c("numeric","numeric","character","factor",
+            #Row        Col         Name      Media
+            "factor","factor",  "factor", "character",
+            #           Temp    Array(1-6)  Condition    Well      
+            "numeric","numeric","numeric", "numeric")
+#             CC          R        MinSize     Corr
+
+modeled_df = read.table(modeled_file,header=T, colClasses=classes,sep="\t")
+
+outlierDF = data.frame(Gene = character(),
+                       Media = factor(),
+                       Temp = factor(),
+                       Array = factor(),
+                       Well = character())
+
+arrays = sort(unique(modeled_df$Array))
+
+conditions = sort(unique(modeled_df$Condition))
+
+for(array in arrays){
+  subArray = subset(modeled_df, df$Array == array)
+  for(condition in conditions){
+    subCondition = subset(subArray, subArray$Condition == condition)
+    
+    names = sort(unique(subCondition$Gene))
+    for(name in names){
+      block = subset(subCondition,subCondition$Gene == name)
+      
+      #script fails if block has too few spots
+      #need to remove blocks with < cutoff spots
+      
+      if(nrow(block) < 3){
+        block = block[,c(3,4,5,6,8)]
+        outlierDF = rbind(outlierDF,block)
+        
+      }else{
+      
+      for(i in 1:nrow(block)){
+        spot = block[i,]
+        omit_block = block[-i,]
+        mean_R = mean(omit_block$R)
+        sd_R = sd(omit_block$R)
+        if(spot$R < (mean_R - 4*sd_R) |
+           spot$R > (mean_R + 4*sd_R)){
+          spot = spot[,c(3,4,5,6,8)]
+          outlierDF = rbind(outlierDF,spot)
+        }}
+      }
+    }
+  }
+}
+
+
+outlier_removed_df = anti_join(modeled_df, outlierDF, by = c("Media" , "Temp", "Array", "Well"))
+
+write.table(outlier_removed_df, "logistic_growth.omit_errors_lowcorr_outlierRate.tab",row.names = F, sep = "\t")
